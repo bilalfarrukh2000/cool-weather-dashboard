e16bf4d8ac740e3e719b858e4c3356f9fd63ec5b
bilalfarrukh2000 <bilalfarrukh2000@gmail.com>
2022-09-28 21:50:05 -0400

    Add function to get current weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,23 @@ var long;
 var lat;
 
 var currentDate=moment().format('l');
+
+//Function to getData
+
+function getData(city){
+
+    var currentCity=city;
+    var url1="https://api.openweathermap.org/data/2.5/weather?q="+currentCity+"&appid=166d9ab93a294c2aa16185a2466084c7";
+    $.ajax({
+      async: false,
+      type: 'GET',
+      url: url1,
+      success: function(data) {
+           cityData=data;
+      }
+    });
+  
+    long=cityData.coord.lon;
+    lat=cityData.coord.lat;
+    getForecastData();
+  }
